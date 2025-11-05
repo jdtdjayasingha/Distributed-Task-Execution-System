d7072bab7b099b493a3f35e6bc78c68961596bb5
jdtdjayasingha <jdtdjayasingha@gmail.com>
2025-11-05 11:09:07 +0530

    Add Master–Worker network visualization

--- main.py
+++ main.py
@@ -13,19 +13,34 @@ def worker_task(x):
 if __name__ == '__main__':
     tasks = [1, 2, 3, 4, 5, 6]
 
-    # Sequential
     start_seq = time.time()
     seq_results = [worker_task(x) for x in tasks]
     end_seq = time.time()
 
-    # Parallel
     start_par = time.time()
     with multiprocessing.Pool(processes=3) as pool:
-        par_results = pool.map(worker_task, tasks)
+        results = pool.map(worker_task, tasks)
     end_par = time.time()
 
     print("Input tasks:", tasks)
-    print("Results:", par_results)
+    print("Results:", results)
     print("Sequential execution time:", f"~{round(end_seq - start_seq, 1)} seconds")
     print("Total parallel execution time:", f"~{round(end_par - start_par, 1)} seconds")
+    print()
+
+    G = nx.DiGraph()
+    G.add_node("Master", color='lightblue')
+    workers = [f"Worker-{i}" for i in range(1, 3 + 1)]
+    for w in workers:
+        G.add_node(w, color='lightgreen')
+        G.add_edge("Master", w, label="Task")
+        G.add_edge(w, "Master", label="Result")
+
+    colors = [nx.get_node_attributes(G, 'color')[node] for node in G.nodes()]
+    pos = nx.spring_layout(G)
+    nx.draw(G, pos, with_labels=True, node_color=colors, node_size=2000, font_size=10, arrows=True)
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, 'label'))
+    plt.title("Distributed Task Execution System")
+    plt.show()
+
 
